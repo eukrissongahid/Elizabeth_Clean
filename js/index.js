7d0f80fdfa24b658f81c749500d4fd1b3290441b
MICROSOURCINGPH\eukris.songahid <eukris.songahid@microsourcing.ph>
2025-05-25 09:42:38 +0800

    created other pages

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module parse failed: 'import' and 'export' may appear only with 'sourceType: module' (1:0)\nFile was processed with these loaders:\n * ./node_modules/laravel-mix/node_modules/babel-loader/lib/index.js\n * ./node_modules/babel-loader/lib/index.js\nYou may need an additional loader to handle the result of these loaders.\n> import Alpine from 'alpinejs';\n| import Swiper from 'swiper';\n| import 'swiper/css';");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './sections/**/*.liquid',
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+};
